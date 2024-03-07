c22bbe13313bf3ba7e5da7e3e8b58d848606d73c
murpheq <neweru1x3@mail.ru>
2024-03-07 13:28:49 +0300

    завершение проекта

--- final_win.py
+++ final_win.py
@@ -4,22 +4,23 @@ from instr import *
 import second_win
 
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+        self.exp = exp
         self.set_appear()
         self.initUI()
         self.show()
-
+        
     def set_appear(self):
         self.setWindowTitle(txt_finalwin)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
 
     def initUI(self):
-        self.lbl_name = QLabel(f'имя: ')
-        self.lbl_age = QLabel(f'возраст: ')
-        self.lbl_index = QLabel(txt_index)
-        self.work_heart = QLabel(txt_workheart)
+        self.lbl_name = QLabel(f'имя: {self.exp.name}')
+        self.lbl_age = QLabel(f'возраст: {self.exp.age}')
+        self.work_heart = QLabel(txt_workheart + self.results())
+        self.lbl_index = QLabel(txt_index + str(self.index))
         
         self.v_line = QVBoxLayout()
         
@@ -32,3 +33,62 @@ class FinalWin(QWidget):
         self.v_line.addWidget(self.work_heart, alignment=Qt.AlignCenter)
 
         self.setLayout(self.v_line)
+
+    def results(self):
+        self.index = (4*(int(self.exp.result1) + int(self.exp.result2) + int(self.exp.result3)) - 200) / 10
+
+        if self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index >= 11:
+                return txt_res2
+            elif self.index >= 6:
+                return txt_res3
+            elif self.index >= 0.5:
+                return txt_res4
+            return txt_res5
+
+        elif self.exp.age >= 13:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index >= 12.5:
+                return txt_res2
+            elif self.index >= 7.5:
+                return txt_res3
+            elif self.index >= 2:
+                return txt_res4
+            return txt_res5
+
+        elif self.exp.age >= 11:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index >= 14:
+                return txt_res2
+            elif self.index >= 9:
+                return txt_res3
+            elif self.index >= 3.5:
+                return txt_res4
+            return txt_res5
+
+        elif self.exp.age >= 9:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index >= 15.5:
+                return txt_res2
+            elif self.index >= 10.5:
+                return txt_res3
+            elif self.index >= 5:
+                return txt_res4
+            return txt_res5
+
+        elif self.exp.age >= 7:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index >= 17:
+                return txt_res2
+            elif self.index >= 12:
+                return txt_res3
+            elif self.index >= 6.5:
+                return txt_res4
+            return txt_res5
+        
--- second_win.py
+++ second_win.py
@@ -1,7 +1,16 @@
 from PyQt5.QtCore import Qt, QTimer, QTime
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QLineEdit, QLCDNumber
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QLineEdit, QLCDNumber, QMessageBox
+from PyQt5.QtGui import QFont
 from instr import *
 from final_win import FinalWin
+
+class Experiment():
+    def __init__(self, name, age, result1, result2, result3):
+        self.name = name
+        self.age = age
+        self.result1 = result1
+        self.result2 = result2
+        self.result3 = result3
 
 class TestWin(QWidget):
     def __init__(self):
@@ -30,21 +39,28 @@ class TestWin(QWidget):
         self.test_1 = QLabel(txt_test1)
         self.btn_begin_1 = QPushButton(txt_starttest1)
         self.result1_input = QLineEdit()
+        self.result1_input.setDisabled(True)
         self.result1_input.setPlaceholderText(txt_hinttest1)
 
         self.test_2 = QLabel(txt_test2)
         self.btn_begin_2 = QPushButton(txt_starttest2)
+        self.btn_begin_2.setDisabled(True)
 
         self.test_3 = QLabel(txt_test3)
         self.btn_begin_3 = QPushButton(txt_starttest3)
+        self.btn_begin_3.setDisabled(True)
         self.result2_input = QLineEdit()
+        self.result2_input.setDisabled(True)
         self.result3_input = QLineEdit()
+        self.result3_input.setDisabled(True)
         self.result2_input.setPlaceholderText(txt_hinttest2)
         self.result3_input.setPlaceholderText(txt_hinttest3)
 
         self.btn_total = QPushButton(txt_sendresults)
 
         self.text_timer = QLabel(txt_timer)
+
+        self.error = QMessageBox()
 
         self.h_line = QHBoxLayout()
 
@@ -80,6 +96,8 @@ class TestWin(QWidget):
     def connects(self):
         self.btn_total.clicked.connect(self.next_click)
         self.btn_begin_1.clicked.connect(self.first_timer)
+        self.btn_begin_2.clicked.connect(self.second_timer)
+        self.btn_begin_3.clicked.connect(self.third_timer)
 
     def first_timer(self):
         global time
@@ -93,11 +111,88 @@ class TestWin(QWidget):
         global time
 
         time = time.addSecs(-1)
-        self.text_timer.setText(time.toString('hh:mm:ss'))
+        self.text_timer.setText(time.toString('mm:ss'))
+        self.text_timer.setFont(QFont('Times', 32, QFont.Bold))
 
-        if time.toString('hh:mm:ss') == '00:00:00':
+        if time.toString('mm:ss') == '00:00':
+            self.btn_begin_2.setDisabled(False)
+            self.btn_begin_1.setDisabled(True)
+
+            self.result1_input.setDisabled(False)
+
             self.timer.stop()
 
+    def second_timer(self):
+        global time
+
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+
+    def timer2Event(self):
+        global time
+
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString('ss'))
+        self.text_timer.setFont(QFont('Times', 32, QFont.Bold))
+
+        if time.toString('ss') == '00':
+            self.btn_begin_3.setDisabled(False)
+            self.btn_begin_2.setDisabled(True)
+            self.timer.stop()
+
+    def third_timer(self):
+        global time
+
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+
+    def timer3Event(self):
+        global time
+
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString('hh:ss'))
+        self.text_timer.setFont(QFont('Times', 32, QFont.Bold))
+
+        if time.toString('hh:ss') == '00:00':
+            self.result3_input.setDisabled(False)
+
+            self.timer.stop()
+        
+        if time.toString('hh:ss') >= '00:45':
+            self.text_timer.setStyleSheet('color: #00FF00')
+        elif time.toString('hh:ss') <= '00:15':
+            self.text_timer.setStyleSheet('color: #00FF00')
+        else:
+            self.text_timer.setStyleSheet('color: #000000')
+            self.result2_input.setDisabled(False)
+
     def next_click(self):
-        self.hide()
-        self.fw = FinalWin()
+        if not self.name_input.text():
+            self.error.setText('Ошибка! Не введено имя!')
+            self.error.show()
+        elif not self.age_input.text() or not self.age_input.text().isdigit() or int(self.age_input.text()) < 7:
+            self.error.setText('Ошибка! Некорректное значение возраста!')
+            self.error.show()
+        
+        elif not self.result1_input.text() or not self.result1_input.text().isdigit() or int(self.result1_input.text()) <= 0:
+            self.error.setText('Ошибка! Некоректное значение первого измерения!')
+            self.error.show()
+
+        elif not self.result2_input.text() or not self.result2_input.text().isdigit() or int(self.result1_input.text()) <= 0:
+            self.error.setText('Ошибка! Некоректное значение второго измерения!')
+            self.error.show()
+
+        elif not self.result3_input.text() or not self.result3_input.text().isdigit() or int(self.result1_input.text()) <= 0:
+            self.error.setText('Ошибка! Некоректное значение третьего измерения!')
+            self.error.show()
+
+        else:
+            self.hide()
+
+            self.exp = Experiment(self.name_input.text(), int(self.age_input.text()), int(self.result1_input.text()),
+            int(self.result2_input.text()), int(self.result3_input.text()))
+            self.fw = FinalWin(self.exp)
